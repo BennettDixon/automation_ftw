53d3fdca0887dfb543b2e633a35ad44810d63e6a
BennettDixon <bennettdixon16@gmail.com>
2019-04-11 18:04:05 +0000

    added more info on how to setup certs using certbot

--- LetsEncryptSSL/install-LE-Client.sh
+++ LetsEncryptSSL/install-LE-Client.sh
@@ -7,3 +7,9 @@
 add-apt-repository ppa:certbot/certbot
 apt-get -y update
 apt-get -y install certbot
+
+# commands for setting up certs
+# MAKE SURE PORT 80 IS NOT IN USE WHEN RUN
+#	-> netstat -na | grep ':80.*LISTEN'
+#	-> sudo service <___service_open_on_80___> stop
+# sudo certbot certonly --standalone --preferred-challenges http -http-01-port 80 -d mydomain.com -d www.mydomain.com
